efde0d5389a3001e1dac40fb2efae0708e5369a6
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-28 22:26:17 -0500

    Created endpoints for get, get by id, and post

--- data/routes/cohortRouter.js
+++ data/routes/cohortRouter.js
@@ -11,9 +11,35 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const newCohort = await db.insert(req.body).into("cohorts");
-    res.status(201).json(newCohort);
+    if (newCohort) {
+      res.status(201).json(newCohort);
+    } else {
+      res.status(400).json({ message: "Could not post into database" });
+    }
   } catch (err) {
-    res.status(500).json({ message: "Error 500 status code lul" });
+    res.status(500).json({
+      message: "Something went wrong with this request (attempt to add post)"
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const getCohort = await db("cohorts")
+      .where({ id: req.params.id })
+      .first();
+    if (getCohort) {
+      res.status(200).json(getCohort);
+    } else {
+      res
+        .status(400)
+        .json({ message: "Cohort with specified ID does not exist." });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message:
+        "Something went wrong with this request (attempt to retrieve cohort by ID"
+    });
   }
 });
 
